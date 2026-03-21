b3cc2718e7e9e6c4623bbd05dc2a367a7a6caba0
Fr33styler <Fr33stylerMC@gmail.com>
2026-03-22 00:27:18 +0200

    Made sessions iterable

--- game-engine-api/src/main/java/ro/fr33styler/gameengine/api/game/session/Sessions.java
+++ game-engine-api/src/main/java/ro/fr33styler/gameengine/api/game/session/Sessions.java
@@ -2,7 +2,7 @@ package ro.fr33styler.gameengine.api.game.session;
 
 import org.jetbrains.annotations.Nullable;
 
-public interface Sessions {
+public interface Sessions extends Iterable<Session> {
 
     @Nullable
     Session getSession(String id);
